89d50205366c14fc0901f40e08cc4fbb84109b73
leocyx <leo8400725@yahoo.com.tw>
2017-11-09 20:44:38 +0800

    travis ci

--- mtrack.js
+++ mtrack.js
@@ -3,21 +3,22 @@ var total = 0
 var eat = {
     'breakfast':55,
     'lunch':95,
-   ' dinner':80
+    'dinner':80,
 }
 
 var play = {
     'movie_ticket':250,
-    'zoo_ticket':140
+    'zoo_ticket':140,
 }
 
 var traffic = {
     "mrt":25,
     "bus":12,
-    "aircraft":1500
+    "aircraft":1500,
 }
 
 
 M.mtrack = function(eat_select, paly_select, traffic_select){
-    
+  total += eat[eat_select] + play[paly_select] + traffic[traffic_select];
+  return total;
 }
